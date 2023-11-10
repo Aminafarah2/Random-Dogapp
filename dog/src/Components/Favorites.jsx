d602357991f0f57cd9c95f1dbe6479cadeb6ffe7
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 21:31:36 +0300

    structured components

---
+++ dog/src/Components/Favorites.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function Favourites() {
+  return (
+    <div>Favourites</div>
+  )
+}
+export default Favourites;
---
+++ dog/src/Components/index.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Favourites from "./Favourites";
