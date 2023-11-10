7b7b32e7d5d97932723d8d9b63fa89fb65f7fa35
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 22:20:30 +0300

    favourite component

--- dog/src/Components/Favourites.jsx
+++ dog/src/Components/Favourites.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from "react";
 
- function Favourites() {
+function Favourites() {
+  // Retrieve favorites from localStorage
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
   return (
-    <div>Favourites</div>
-  )
+    <div className="container mt-5">
+      <h1 className="mb-4">Hello from Favourites</h1>
+      <div className="row">
+        {favorites.map((image, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <div className="card">
+              <img src={image} alt={`Favorite Dog ${index + 1}`} className="card-img-top" />
+              <div className="card-body">
+                {/* Add any additional content or buttons inside the card-body */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Favourites;
+
