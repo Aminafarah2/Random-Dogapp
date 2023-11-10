be7e9d7ef8594cafe9c83735b0c62356158582b8
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 22:18:40 +0300

    about component

--- dog/src/Components/About.jsx
+++ dog/src/Components/About.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+function About ({ dogImages }){
+    const DisplayDogs =[
+        { id: 1,  imageUrl:"https://i.pinimg.com/236x/73/66/13/736613fdd36b0f711208483155f3abd7.jpg" },
+        {id: 2, imageUrl:"https://i.pinimg.com/236x/e9/2a/2a/e92a2a9e9af0ab81b1b7988507fb9a8f.jpg"},
+        {id: 3,  imageUrl:"https://i.pinimg.com/236x/d0/cf/d7/d0cfd7f2ed6f40d16ec0f59bd90c78b2.jpg"},
+        
+    ]
+     const navigate = useNavigate();
+    const handleExploreClick = () => {
+        navigate("/home");
+      };
 
- function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div>
+            <h1>RANDOM DOGS </h1>
+            <button
+          className="btn btn-dark btn-lg position-relative py-2 px-4 text-bg-secondary border border-secondary rounded-pill"
+          onClick={() => handleExploreClick()}
+        >
+          LOG IN
+        </button>
+        <div className="container mt-4">
+        <h2 className="text-center">Explore Cute Dogs</h2>
+        <p>this is an app for random Images </p>
+        <div className="row">
+            { DisplayDogs .map((dog) => (
+            <div key={dog.id} className="col-md-4 mb-4">
+              <div className="card">
+                
+                <img
+                  src={dog.imageUrl}
+                  className="card-img-top"
+                  alt={dog.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{dog.name}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+          
+        </div>
+      </div>
+        </div>
+    )
 }
 export default About;
--- dog/src/Components/index.jsx
+++ dog/src/Components/index.jsx
@@ -4,34 +4,36 @@ import About from "./About";
 import Home from "./Home";
 import Favourites from "./Favourites";
 
-function Components (){
+function Components() {
     const [dogImages, setDogImages] = useState([]);
-  const [page] = useState(1);
-
-  useEffect(() => {
-    fetch(`https://dog.ceo/api/breeds/image/random/10?page=${page}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status === "success") {
-          setDogImages([...dogImages, ...data.message]);
-        } else {
-          alert("Failed to fetch dog images.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching dog images:", error);
-      });
-  }, [page]);
+    const [page] = useState(1);
+  
+    useEffect(() => {
+      fetch(`https://dog.ceo/api/breeds/image/random/10?page=${page}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === "success") {
+            setDogImages([...dogImages, ...data.message]);
+          } else {
+            alert("Failed to fetch dog images.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching dog images:", error);
+        });
+    }, [page]);
+  
     return (
-        <BrowserRouter>
-          <Routes>
-          <Route path="/" element={<About  />} />
-            <Route path="favourites" element={<Favourites  />} />
-            <Route path="home" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      );
-    }
-    
-    export default Components;
-    
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<About dogImages={dogImages} />} />
+          <Route path="favourites" element={<Favourites dogImages={dogImages} />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  export default Components;
+  
+  
