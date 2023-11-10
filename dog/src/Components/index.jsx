8530fa45f421c9dee09ca0ac45942d39a94ae1cb
aminafarah <amishkamohamed563@gmail.com>
2023-11-10 22:09:01 +0300

    fetching the data

--- dog/src/Components/index.jsx
+++ dog/src/Components/index.jsx
@@ -5,6 +5,23 @@ import Home from "./Home";
 import Favourites from "./Favourites";
 
 function Components (){
+    const [dogImages, setDogImages] = useState([]);
+  const [page] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random/10?page=${page}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          setDogImages([...dogImages, ...data.message]);
+        } else {
+          alert("Failed to fetch dog images.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching dog images:", error);
+      });
+  }, [page]);
     return (
         <BrowserRouter>
           <Routes>
